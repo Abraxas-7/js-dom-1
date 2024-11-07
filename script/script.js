d36c660565387795f68b2d4d3adcaf5aeac93c06
Abraxas-7 <arber.beshaj.02@gmail.com>
2024-11-07 16:10:03 +0100

    arrow + sintassi più corretta

--- script/script.js
+++ script/script.js
@@ -1,9 +1,9 @@
-document.getElementById('toggleButton').addEventListener('click', function() {
+document.getElementById('toggleButton').addEventListener('click', () => {
     let lampadina = document.getElementById('lampadina');
 
-    if (lampadina.src.includes("img/white_lamp.png")) {
-        lampadina.src = "img/yellow_lamp.png";
+    if (lampadina.getAttribute('src').includes("img/white_lamp.png")) {
+        lampadina.setAttribute('src', "img/yellow_lamp.png");  // Cambia l'immagine con la lampadina gialla
     } else {
-        lampadina.src = "img/white_lamp.png";
+        lampadina.setAttribute('src', "img/white_lamp.png");  // Cambia l'immagine con la lampadina bianca
     }
 });
